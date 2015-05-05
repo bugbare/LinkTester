598a021617a7349737105d2791c9d8f9a0623821
Rishi Shah <rishi.shah@cityindex.com>
2015-05-05 17:56:25 +0100

    Signed-off-by: Rishi Shah <rishi.shah@cityindex.com>

--- CrudXL/Form1.cs
+++ CrudXL/Form1.cs
@@ -26,6 +26,7 @@ namespace CrudXL
 {
     public partial class Form1 : Form
     {
+        /* Initialise the implemented Form, Form1 interface object */
         public Form1()
         {
             InitializeComponent();
@@ -33,18 +34,27 @@ namespace CrudXL
 
         private void createButton_Click(object sender, EventArgs e)
         {
-            /* Instantiate Excel App object to use to generate an excell workbook*/
+            /* Instantiate Excel App object to use to generate an excel workbook*/
             Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            /* Creata a local String object to hold the pageUrl to be entered subsequently via the form objects*/
             String pageUrl;
+
+            /* Check that the host system has excel installed, if not output an error message, with only an exit confirmation button*/
 
             if (xlApp == null)
             {
-                MessageBox.Show("Excel is not properly installed!!");
+                if (MessageBox.Show("Excel is not properly installed!!", "No Excel Found - Application will Exit", MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk,
+                    MessageBoxDefaultButton.Button1) == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
                 return;
             }
 
+            /* If there is no user input for the URL test field then default to City Index UK site */
 
-           if (this.inputPageUrl.Text == "") 
+            if (this.inputPageUrl.Text == "") 
                 {
                     pageUrl = "http://www.cityindex.co.uk";
                 }
@@ -54,54 +64,158 @@ namespace CrudXL
                 }
 
             /* Instantiate all excel app objects to contain all the excel workbook components*/
+
             Excel.Workbook xlWorkBook;
             Excel.Worksheet xlWorkSheet;
+
             /* a Value type missing object is required when generating a Workbook using the Excel.Workbook.SaveAs serializing method
-             * to ignore certain options that are not required to be used - null is not allowed to be used instead */
+             * to ignore certain options that are not required to be used - null is not allowed to be used! */
+
             object misValue = System.Reflection.Missing.Value;
+
             /* Instantiate a workbook and initialise the first worksheet, assigned to the xlWorksheet object*/
             xlWorkBook = xlApp.Workbooks.Add(misValue);
             xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
             
             /* Initialise a new webdriver driver object and go to the initial landing page*/
             IWebDriver driver = new FirefoxDriver();
-            driver.Navigate().GoToUrl(pageUrl);
-            string pageTitle = driver.Title;
+
+            /* Setup a headless http client handler that does not allow autoredirects, 
+             * so we get the initial response status code returned by the server*/
+
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            httpClientHandler.AllowAutoRedirect = false;
+
+            HttpResponseMessage response;
+
+            /* Instantiate a new Http Client object and apply the attributes of the previously generated http client handler to it */
+            HttpClient client = new HttpClient(httpClientHandler);
+            /* Create a file */
             string resultFile = string.Format("Test Data-{0:yyyy-MM-dd_hh-mm-ss-tt}.xls",
                                 DateTime.Now);
-            /* Create a local variable to hold contents of the page source*/
 
-            var pageLinks = new List<string>();
-            var anchors = driver.FindElements(By.TagName("a"));
-            int rCnt = 1;
-            string lPattern = "http";
-            foreach (var element in anchors)
-            {
-                if (element.GetAttribute("href") != null)
+            try
+            {            /* use firefox (driver) to navigate to the initial page sent by user or default value provided by app */
+                driver.Navigate().GoToUrl(pageUrl);
+
+                /* Create a local variable to hold contents of the page source*/
+                var pageLinks = new List<string>();
+
+                /* Get all attribute elements on the page and store in a locally declared variable */
+                var anchors = driver.FindElements(By.TagName("a"));
+
+                /* Add a header row to the first row in the excel worksheet*/
+                xlWorkSheet.Cells[1, 1] = "SOURCE URL ";
+                xlWorkSheet.Cells[1, 2] = "DESTINATION URL";
+                xlWorkSheet.Cells[1, 3] = "HTTP RESPONSE CODE";
+                xlWorkSheet.Cells[1, 4] = "DESTINATION PAGE TITLE";
+
+                /* Rest the row count to 2, so that it will start adding entries after the header row in the excel worksheet */
+                int rCnt = 2;
+
+                /* Create a search pattern to filter attributes by content contianing http*/
+                string lPattern = "http";
+                foreach (var element in anchors)
+                /* filter attributes further by their category = href */
                 {
-                    string pageLink = element.GetAttribute("href");
-         
-                    
-                    if (System.Text.RegularExpressions.Regex.IsMatch(pageLink, lPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                    if (element.GetAttribute("href") != null)
+
+                    /* store the value of each href attribute into a pagelink list string object */
                     {
-                        if (System.Text.RegularExpressions.Regex.IsMatch(pageLink, "/#", System.Text.RegularExpressions.RegexOptions.IgnoreCase) == false)
+                        string pageLink = element.GetAttribute("href");
+
+                        /* Filter the list objects by the http search pattern, so only legitimate http links are stored */
+                        if (System.Text.RegularExpressions.Regex.IsMatch(pageLink, lPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
                         {
-                            pageLinks.Add(pageLink);
-                            xlWorkSheet.Cells[rCnt, 1] = pageLink;
-                            rCnt++;
+                            /* Filter the list objects further to remove any that contain a script call "/#" */
+                            if (System.Text.RegularExpressions.Regex.IsMatch(pageLink, "/#", System.Text.RegularExpressions.RegexOptions.IgnoreCase) == false)
+                            {
+                                pageLinks.Add(pageLink);
+                                xlWorkSheet.Cells[rCnt, 1] = pageLink;
+                                rCnt++;
+                            }
                         }
                     }
                 }
-            }
 
-            rCnt = 1;
-            foreach (var link in pageLinks)
+                /* Rest the row count to the second row on the excel sheet, so that values 
+                 * that result from navigating to the value in the first cell of that row 
+                 * Can be stored in proceeding columns in the same row */
+
+                rCnt = 2;
+
+                /* Use a foreach loop to cycle through all objects in the pageLinks list */
+                foreach (var link in pageLinks)
+                {
+
+                    try
+                    {
+                        /* store the http response header retrieved from navigating using http 
+                         * client to the http link provided by the current pageList object */
+                        response = client.GetAsync(pageUrl).Result;
+                        /* 
+                         * DEBUG MessageBox.Show("Response Status headers: " + response.Headers, "STATUS CODE", MessageBoxButtons.OK); 
+                         */
+
+                        /* Navigate to the URL stored in the current pageList object */
+                        driver.Navigate().GoToUrl(link);
+
+                        /* retreive and store the current URL value */
+                        string destinationUrl = driver.Url;
+
+                        /* retrieve and store the current URL Page Title */
+                        string pageTitle = driver.Title;
+
+                        /* write out values retrieved after page navigation to excel worksheet in same row as source URL */
+                        xlWorkSheet.Cells[rCnt, 2] = destinationUrl;
+                        xlWorkSheet.Cells[rCnt, 3] = response.StatusCode;
+                        xlWorkSheet.Cells[rCnt, 4] = pageTitle;
+
+                        /* Return to source page */
+                        //driver.Navigate().Back();
+                        rCnt++;
+
+                    }
+                    catch (Exception Timeout)
+                    {
+                        xlWorkSheet.Cells[rCnt, 2] = driver.Url;
+                        xlWorkSheet.Cells[rCnt, 3] = "TIMEOUT FAILURE";
+                        xlWorkSheet.Cells[rCnt, 4] = driver.Title;
+                        driver.Navigate().Back();
+                        rCnt++;
+                        MessageBox.Show(
+                        Timeout.ToString()
+                        + "\nFound before ROW: "
+                        + rCnt.ToString(),
+                        "********************TIMEOUT********************",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ae)
             {
-                driver.Navigate().GoToUrl(link);
-                var destinationUrl = driver.Url;
-                xlWorkSheet.Cells[rCnt, 2] = destinationUrl;
-                driver.Navigate().Back();
-                rCnt++;
+                if (MessageBox.Show(               
+                    ae.ToString(),
+                    "********************EXCEPTION********************",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1) == DialogResult.Cancel)
+                {
+                    xlWorkBook.Close(true, null, null);
+                    xlWorkBook.SaveAs(resultFile, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                    xlWorkBook.Close(true, misValue, misValue);
+                    xlApp.Quit();
+                    driver.Quit();
+
+                    releaseObject(xlWorkSheet);
+                    releaseObject(xlWorkBook);
+                    releaseObject(xlWorkSheet);
+                    releaseObject(xlWorkBook);
+                    releaseObject(xlApp);
+
+                    Application.Exit();
+                }
             }
 
 
@@ -153,15 +267,6 @@ namespace CrudXL
                 } else {
                        redirectState = HttpStatusCode.MovedPermanently;
                     }
-
-/*            if (sPattern == "") {
-                sPattern = "http://www.cityindex.co.uk";
-                MessageBox.Show("Site to test: " + sPattern);
-            }
-            else
-            {
-                MessageBox.Show("Site to test: " + sPattern);
-            }*/
 
             
             DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
@@ -218,7 +323,7 @@ namespace CrudXL
                 object misValue = System.Reflection.Missing.Value;
                 xlWorkBookNew = xlApp.Workbooks.Add(misValue);
                 xlWorkSheetNew = (Excel.Worksheet)xlWorkBookNew.Worksheets.get_Item(1);
-                xlWorkSheetNew.Cells[1, 1] = "SOURCE URL: ";
+                xlWorkSheetNew.Cells[1, 1] = "SOURCE URL ";
                 xlWorkSheetNew.Cells[1, 2] = "EXPECTED DESTINATION URL";
                 xlWorkSheetNew.Cells[1, 3] = "ACTUAL DESTINATION URL";
                 xlWorkSheetNew.Cells[1, 4] = "EXPECTED HTTP RESPONSE CODE";
